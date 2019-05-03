b94d33c44b173ef923b876a8463a47d95774bba5
Kalle Pusa <kalle.pusa@gmail.com>
2019-05-03 16:07:26 +0300

    Added unique constraint. Cleared db.

--- application/__init__.py
+++ application/__init__.py
@@ -79,6 +79,7 @@ from application.models.models import User
 from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
+db.drop_all()
 
 try: 
     db.create_all()
--- application/courses/views.py
+++ application/courses/views.py
@@ -77,7 +77,7 @@ def course_add_dislike(course_id):
 
 @app.route("/course/taken<course_id><student_id>", methods=["POST"])
 def mark_course_as_taken(course_id, student_id):
-    stmt = text("INSERT INTO Course_Student(course_id, student_id) VALUES(:course_id, :student_id)").params(course_id=course_id, student_id=student_id)
+    stmt = text("INSERT OR IGNORE INTO Course_Student(course_id, student_id) VALUES(:course_id, :student_id)").params(course_id=course_id, student_id=student_id)
     db.session().execute(stmt)
     db.session().commit()
 
@@ -85,7 +85,7 @@ def mark_course_as_taken(course_id, student_id):
 
 @app.route("/course/mycourse/<student_id>")
 def show_my_courses(student_id):
-    stmt = text("SELECT DISTINCT Course.name FROM COURSE_STUDENT JOIN COURSE ON Course.id = Course_Student.course_id WHERE student_id = :student_id ").params(student_id=student_id)
+    stmt = text("SELECT Course.name FROM COURSE_STUDENT JOIN COURSE ON Course.id = Course_Student.course_id WHERE student_id = :student_id ").params(student_id=student_id)
     res = db.engine.execute(stmt)
     response = []
     for row in res:
